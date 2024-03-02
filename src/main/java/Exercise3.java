2c5046aea493a1e6a2156bc6b0ae147047649588
kouroshMojdehi <k.rashidimojdehi@gmail.com>
2024-03-02 03:33:40 +0330

    Exercise3 tested commited

--- src/main/java/Exercise3.java
+++ src/main/java/Exercise3.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.lang.String;
 
 public class Exercise3 {
 
@@ -11,15 +12,13 @@ public class Exercise3 {
     */
 
     public static String extractURL(String text) {
-        String regex = "write your regex pattern here!";  // TODO
-
+        String regex = "(https://|http://)[(a-z)._-]*[^\\s]+";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(text);
 
         if (matcher.find()) {
             return matcher.group();
-        }
-        else{
+        } else {
             return null;
         }
     }
@@ -29,7 +28,14 @@ public class Exercise3 {
      */
 
     public static boolean validateEmail(String email) {
-        // TODO
+        String regex = "[\\w.-_\\d]+@[\\w.-_]+.[com|org|ir][^\\s]+";
+//        String regex = "[\\w.-_\\d]+@[\\w.-_].[com|org|ir]";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+        if (matcher.find()) {
+            return true;
+        }
         return false;
     }
 
@@ -39,23 +45,35 @@ public class Exercise3 {
 
     public static List<String> findWordsWithRepeatLetters(String input) {
         List<String> wordsWithRepeatLetters = new ArrayList<>();
-        return wordsWithRepeatLetters;
-        // TODO
-    }
+        String regex = "\\w*(\\w)\\w*\\1+\\w*";
+        // the \1 refers to the letter which is in \\w
 
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        while (matcher.find()) {
+            wordsWithRepeatLetters.add(matcher.group());
+        }
+
+        return wordsWithRepeatLetters;
+
+
+    }
     /*
     Bonus Problem ;)
     implement the method below so that it returns a list of words that are repeated twice accidentally in a string
     for example: "appleapple orange pearpear pineapple" -> ["appleapple", "pearpear"]
     */
 
-    public static List<String> findReapetdWords(String input) {
-        List<String> repeatedWords = new ArrayList<>();
-        return repeatedWords;
-        // TODO
-    }
+        public static List<String> findReapetdWords (String input){
+            List<String> repeatedWords = new ArrayList<>();
 
-    public static void main(String[] args) {
-        // test your code here!
-    }
-}
+            return repeatedWords;
+
+        }
+
+        public static void main (String[]args){
+            // test your code here!
+
+            }
+        }
+
